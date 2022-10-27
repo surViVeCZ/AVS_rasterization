a4694b34b07ef3fb095e6560440ba153a8fdd298
Petr Pou4 <poucpetr11@seznam.cz>
2022-10-27 22:38:48 +0200

    mendelbrot

---
+++ build/CMakeFiles/mandelbrot.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/poucp/Desktop/schooll/1MIT/AVS/calculators/BaseMandelCalculator.cc" "/mnt/c/Users/poucp/Desktop/schooll/1MIT/AVS/build/CMakeFiles/mandelbrot.dir/calculators/BaseMandelCalculator.cc.o"
+  "/mnt/c/Users/poucp/Desktop/schooll/1MIT/AVS/calculators/BatchMandelCalculator.cc" "/mnt/c/Users/poucp/Desktop/schooll/1MIT/AVS/build/CMakeFiles/mandelbrot.dir/calculators/BatchMandelCalculator.cc.o"
+  "/mnt/c/Users/poucp/Desktop/schooll/1MIT/AVS/calculators/LineMandelCalculator.cc" "/mnt/c/Users/poucp/Desktop/schooll/1MIT/AVS/build/CMakeFiles/mandelbrot.dir/calculators/LineMandelCalculator.cc.o"
+  "/mnt/c/Users/poucp/Desktop/schooll/1MIT/AVS/calculators/RefMandelCalculator.cc" "/mnt/c/Users/poucp/Desktop/schooll/1MIT/AVS/build/CMakeFiles/mandelbrot.dir/calculators/RefMandelCalculator.cc.o"
+  "/mnt/c/Users/poucp/Desktop/schooll/1MIT/AVS/common/cnpy.cc" "/mnt/c/Users/poucp/Desktop/schooll/1MIT/AVS/build/CMakeFiles/mandelbrot.dir/common/cnpy.cc.o"
+  "/mnt/c/Users/poucp/Desktop/schooll/1MIT/AVS/main.cc" "/mnt/c/Users/poucp/Desktop/schooll/1MIT/AVS/build/CMakeFiles/mandelbrot.dir/main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../common"
+  "../calculators"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/mandelbrot.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mandelbrot.dir/calculators/BaseMandelCalculator.cc.o"
+  "CMakeFiles/mandelbrot.dir/calculators/BatchMandelCalculator.cc.o"
+  "CMakeFiles/mandelbrot.dir/calculators/LineMandelCalculator.cc.o"
+  "CMakeFiles/mandelbrot.dir/calculators/RefMandelCalculator.cc.o"
+  "CMakeFiles/mandelbrot.dir/common/cnpy.cc.o"
+  "CMakeFiles/mandelbrot.dir/main.cc.o"
+  "mandelbrot.pdb"
+  "mandelbrot"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mandelbrot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
