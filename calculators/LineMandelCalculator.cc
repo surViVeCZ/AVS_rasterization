89fde1bc575c32553152a41e8f01a78dd186d54c
surViVeCZ <poucpetr11@seznam.cz>
2022-10-31 22:21:44 +0100

    aligned pragma

--- calculators/LineMandelCalculator.cc
+++ calculators/LineMandelCalculator.cc
@@ -42,6 +42,9 @@ int *LineMandelCalculator::calculateMandelbrot() {
         }
         #pragma omp simd reduction(+:sum)
         for (int k = 0; k < limit; k++) {
+
+            #pragma omp simd aligned(zReal:sizeof(zReal))
+            #pragma omp simd aligned(zImag:sizeof(zImag))
             for (int j = 0; j < width; j++) {
                 float x = x_start + j * dx;  // current real value
                 float r2 = zReal[j] * zReal[j];
